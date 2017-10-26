3d43af044de4a2deada099f2d453b404398426ba
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2017-10-26 20:04:18 +0200

    Paginacion version 1

--- Ejercicios/src/EjercicioBeautifulSoup/EjercicioJosema.py
+++ Ejercicios/src/EjercicioBeautifulSoup/EjercicioJosema.py
@@ -6,6 +6,7 @@ import tkMessageBox
 import sqlite3
 from bs4 import BeautifulSoup
 import requests #pip install requests
+from numpy.f2py.crackfortran import previous_context
 
 def almacenar_categoria():
     conn = sqlite3.connect('comida.db')
@@ -40,7 +41,7 @@ def almacenar_productos():
          NOMBRE            TEXT     NOT NULL,
          lINK           TEXT     NOT NULL,
          PRICE           TEXT    NOT NULL,
-         NEWPRICE        TEXT);''')
+         OLDPRICE        TEXT);''')
     cursor = conn.execute("SELECT * FROM CATEGORIAS")
     
     for row in cursor:
@@ -48,25 +49,38 @@ def almacenar_productos():
         data = r.text
         soup = BeautifulSoup(data, "lxml")
         
-        productos = soup.find_all("div",{"class","prod_wrap"})
-        for p in productos:
-            link = p.find("div",{"class","prod_name"}).find("a")
-            nombre = link.get_text()
-            link = link["href"]
-            price = p.find("span",{"class","product_preu"})
-            old_price = price.find("del")
-            if old_price != None:
-                new_price=old_price.get_text()
-                real_price=price.get_text().replace(new_price,"")
-            else:
-                new_price=0.0
-                real_price=price.get_text()
-            conn.execute("INSERT INTO PRODUCTOS VALUES(?,?,?,?,?);",(row[0],nombre,link,real_price, new_price))
+        paginas=soup.find("ul",{"class","pagination"})
+        lastPage=1
+        if paginas != None:
+            lastPage = int(paginas.get_text()[-1])
+        current=1
+        while current<=lastPage:
+#             almacenar_producto(lastPage,row[1], row[0]) #AQUIIIIIIIIIIII
+            url = row[1]+u'?p='+str(current).decode('utf8')
+#             print data
+            r=requests.get(url)
+            data = r.text
+            soup = BeautifulSoup(data, "lxml")
+     
+            productos = soup.find_all("div",{"class","prod_wrap"})
+            for p in productos:
+                link = p.find("div",{"class","prod_name"}).find("a")
+                nombre = link.get_text()
+                link = link["href"]
+                price = p.find("span",{"class","product_preu"})
+                old_price = price.find("del")
+                if old_price != None:
+                    previous_price=old_price.get_text()
+                    real_price=price.get_text().replace(previous_price,"")
+                else:
+                    previous_price=0.0
+                    real_price=price.get_text()
+                conn.execute("INSERT INTO PRODUCTOS VALUES(?,?,?,?,?);",(row[0],nombre,link,real_price, previous_price))
+            current+=1
     
     conn.commit()
     conn.close()
     tkMessageBox.showinfo( "Informacion", "Base de Datos creada correctamente")
-
     
 def mostar_categoria():
     lc=[]
@@ -80,7 +94,7 @@ def mostar_categoria():
     
     spin = Spinbox(new,values=categorias)
     
-    text = Tkinter.Text(new)
+    text = Tkinter.Listbox(new, width=100, height=20, selectmode= EXTENDED)
     scrollbar = Tkinter.Scrollbar(new)
     
     butt=Tkinter.Button(new, text="buscar", command= lambda: search(spin.get(), text))
@@ -98,10 +112,12 @@ def mostar_categoria():
 def search(categoria, text):
     conn = sqlite3.connect('comida.db')
     cursor = conn.execute("SELECT * FROM PRODUCTOS WHERE CATEGORY LIKE '%"+categoria+"%'")
-    text.delete('1.0', END)
+#     text.delete('1.0', END)
+    i=1
     for row in cursor:
-        text.insert(INSERT,"Producto: "+row[1].strip()+"\n")
-        text.insert(INSERT,"Precio: "+row[3]+"\n")
+        text.insert(i,"Producto: "+row[1].strip()+"\n")
+        text.insert(i+1,"Precio: "+row[3]+"\n")
+        i+=2
     
     conn.close()
 
