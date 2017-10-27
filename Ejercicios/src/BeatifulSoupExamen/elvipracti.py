5255c618888814773b289f414e1e6da4bd99d730
Elvira G. <elviragruiz@gmail.com>
2017-10-27 09:19:41 +0200

    Datos completado

--- Ejercicios/src/BeatifulSoupExamen/elvipracti.py
+++ Ejercicios/src/BeatifulSoupExamen/elvipracti.py
@@ -10,7 +10,33 @@ from numpy.f2py.crackfortran import previous_context
 
 def donothing():
     print "hola"
+
+
+def lista():
+    conn = sqlite3.connect('derecho.db')
+#    cursor = conn.execute("SELECT * from TEMAS")
+
+    lista = Toplevel()
+    barra = Scrollbar(lista)
+    lista = Listbox(lista,width=150,height=40)
     
+    i = 0
+    #TODO este es el que tiene que cambiar:
+ #   for row in cursor:
+    while i < 3:
+        i+=1
+#        lista.insert(i,row[0]+"   Autores: "+row[2]+"  Fecha: "+row[3])
+        lista.insert(i,"HOLA!")
+        
+    lista.pack(side = LEFT, fill = BOTH)
+    barra.pack(side = RIGHT, fill = Y)
+    barra.config( command = lista.yview )
+    lista.mainloop()
+    conn.close()
+
+def mostrar():
+    lista("SELECT * from TEMAS")
+
 
 def principal():
     top = Tkinter.Tk()
@@ -19,8 +45,8 @@ def principal():
     
     dm = Menu(menubar, tearoff=0)
     dm.add_command(label="Cargar", command=donothing)
-    dm.add_command(label="Mostrar", command=donothing)
-    dm.add_command(label="Salir", command=donothing)
+    dm.add_command(label="Mostrar", command=mostrar)
+    dm.add_command(label="Salir", command=top.destroy)
     menubar.add_cascade(label="Datos", menu=dm)
     
     bm = Menu(menubar, tearoff=0)
