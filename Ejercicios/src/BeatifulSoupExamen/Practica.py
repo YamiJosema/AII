4b07d75efb796d829a50277e14a7a708cc8fc032
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2017-10-27 08:40:15 +0200

    Tkinter

--- Ejercicios/src/BeatifulSoupExamen/Practica.py
+++ Ejercicios/src/BeatifulSoupExamen/Practica.py
@@ -1,5 +1,33 @@
-'''
-Created on 27 oct. 2017
+#encoding: utf-8
 
-@author: Josema
-'''
+import Tkinter
+from Tkinter import *
+import tkMessageBox
+import sqlite3
+from bs4 import BeautifulSoup
+import requests #pip install requests
+from numpy.f2py.crackfortran import previous_context
+
+def datos():
+    conn = sqlite3.connect('eventos.db')
+    conn.execute('''DROP TABLE IF EXISTS CATEGORIAS''')
+    conn.execute('''CREATE TABLE CATEGORIAS
+         (NAME         TEXT    NOT NULL,
+         lINK           TEXT     NOT NULL,
+         CATEGORY        TEXT    NOT NULL);''')
+    
+    url = "http://www.sevillaguia.com/sevillaguia/agendacultural/agendacultural.asp"
+    r=requests.get(url)
+    data = r.text
+
+
+top = Tkinter.Tk()
+ 
+D = Tkinter.Button(top, text ="Datos")
+B = Tkinter.Button(top, text ="Buscar")
+E = Tkinter.Button(top, text ="Estadisticas") 
+
+D.pack( side = LEFT )
+B.pack( side = LEFT )
+E.pack( side = LEFT )
+top.mainloop()
