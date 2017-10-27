3e698e0ead507c0efdc2b2ce89c5d3a307e5b4b0
Elvira G. <elviragruiz@gmail.com>
2017-10-27 08:58:51 +0200

    main

--- Ejercicios/src/BeatifulSoupExamen/elvipracti.py
+++ Ejercicios/src/BeatifulSoupExamen/elvipracti.py
@@ -10,34 +10,39 @@ from numpy.f2py.crackfortran import previous_context
 
 def donothing():
     print "hola"
+    
 
-top = Tkinter.Tk()
- 
-menubar = Menu(top)
-
-dm = Menu(menubar, tearoff=0)
-dm.add_command(label="Cargar", command=donothing)
-dm.add_command(label="Mostrar", command=donothing)
-dm.add_command(label="Salir", command=donothing)
-menubar.add_cascade(label="Datos", menu=dm)
-
-bm = Menu(menubar, tearoff=0)
-bm.add_command(label="Tema", command=donothing)
-bm.add_command(label="Autor", command=donothing)
-bm.add_command(label="Fecha", command=donothing)
-
-menubar.add_cascade(label="Buscar", menu=bm)
-
-em = Menu(menubar, tearoff=0)
-em.add_command(label="Temas más populares", command=donothing)
-em.add_command(label="Temas más activos", command=donothing)
-menubar.add_cascade(label="Estadísticas", menu=em)
-
-
-top.config(menu=menubar)
-
-D = Tkinter.Button(top, text ="Datos")
-B = Tkinter.Button(top, text ="Buscar")
-E = Tkinter.Button(top, text ="Estadisticas") 
-
-top.mainloop()
+def principal():
+    top = Tkinter.Tk()
+     
+    menubar = Menu(top)
+    
+    dm = Menu(menubar, tearoff=0)
+    dm.add_command(label="Cargar", command=donothing)
+    dm.add_command(label="Mostrar", command=donothing)
+    dm.add_command(label="Salir", command=donothing)
+    menubar.add_cascade(label="Datos", menu=dm)
+    
+    bm = Menu(menubar, tearoff=0)
+    bm.add_command(label="Tema", command=donothing)
+    bm.add_command(label="Autor", command=donothing)
+    bm.add_command(label="Fecha", command=donothing)
+    
+    menubar.add_cascade(label="Buscar", menu=bm)
+    
+    em = Menu(menubar, tearoff=0)
+    em.add_command(label="Temas más populares", command=donothing)
+    em.add_command(label="Temas más activos", command=donothing)
+    menubar.add_cascade(label="Estadísticas", menu=em)
+    
+    
+    top.config(menu=menubar)
+    
+    D = Tkinter.Button(top, text ="Datos")
+    B = Tkinter.Button(top, text ="Buscar")
+    E = Tkinter.Button(top, text ="Estadisticas") 
+    
+    top.mainloop()
+    
+if __name__=="__main__":
+    principal()
