d9ee5a0dcdd26a198c0eed64ae7be9d8692abd6c
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2017-10-27 09:57:31 +0200

    Finish 1 y 2

--- Ejercicios/src/BeatifulSoupExamen/elvipracti.py
+++ Ejercicios/src/BeatifulSoupExamen/elvipracti.py
@@ -13,21 +13,6 @@ def donothing():
 
 
 def cargar():
-    for i in range(4):
-        url = "https://foros.derecho.com/foro/20-Derecho-Civil-General/page"+str(i)
-        cargar_pagina(url)
-    conn = sqlite3.connect('derecho.db')
-    cursor = conn.execute("SELECT COUNT(*) FROM TEMAS")
-    
-    number=0
-    for c in cursor:
-        number=c[0]
-        print number
-    
-    tkMessageBox.showinfo( "Informacion", "Numero de elementos: "+str(number))
-
-
-def cargar_pagina(url):
     conn = sqlite3.connect('derecho.db')
     conn.execute('''DROP TABLE IF EXISTS TEMAS''')
     conn.execute('''CREATE TABLE TEMAS
@@ -38,31 +23,38 @@ def cargar_pagina(url):
          RES        TEXT    NOT NULL,
          VIS        TEXT    NOT NULL);''')
     
-    url = "https://foros.derecho.com/foro/20-Derecho-Civil-General"
-    r=requests.get(url)
-    data = r.text
+    for i in range(1,4,1):
+        url = "https://foros.derecho.com/foro/20-Derecho-Civil-General/page"+str(i)
+        r=requests.get(url)
+        data = r.text
     
-    soup = BeautifulSoup(data, "lxml")
-    temas = soup.find_all("li",{"class":"threadbit"})
+        soup = BeautifulSoup(data, "lxml")
+        temas = soup.find_all("li",{"class":"threadbit"})
     
-    for t in temas:
-        first=t.find("h3",{"class":"threadtitle"})
-        title = first.get_text()
-        link = first.find("a")["href"]
+        for t in temas:
+            first=t.find("h3",{"class":"threadtitle"})
+            title = first.find("a").get_text()
+            link = first.find("a")["href"]
         
-        second = t.find("ul",{"class":"threadstats"})
-        res=second.find("a").get_text()
-        vis=second.get_text().split("\n")[2].replace("Visitas: ", "")
+            second = t.find("ul",{"class":"threadstats"})
+            res=second.find("a").get_text()
+            vis=second.get_text().split("\n")[2].replace("Visitas: ", "")
         
-        third=t.find("span",{"class":"time"}).parent
-        date = third.get_text().strip()
-        autor = t.find("a",{"class":"username"}).get_text()
+            third=t.find("span",{"class":"time"}).parent
+            date = third.get_text().strip()
+            autor = t.find("a",{"class":"username"}).get_text()
         
-        conn.execute("INSERT INTO TEMAS VALUES(?,?,?,?,?,?);",(title,link,autor,date,res,vis))
+            conn.execute("INSERT INTO TEMAS VALUES(?,?,?,?,?,?);",(title,link,autor,date,res,vis))
+            conn.commit()
+    cursor = conn.execute("SELECT COUNT(*) FROM TEMAS")
     
-    conn.commit()
+    number=0
+    for c in cursor:
+        number=c[0]
+    
+    tkMessageBox.showinfo( "Informacion", "Numero de elementos: "+str(number))
+    
     conn.close()
-
 
 
 def lista(sql):
@@ -89,7 +81,7 @@ def mostrar():
     lista("SELECT * from TEMAS")
 
 def autor():
-    conn = sqlite3.connect('noticias.db')
+    conn = sqlite3.connect('derecho.db')
     busqueda = Toplevel()
     label= Label(busqueda, text="Introduzca el autor:")
     entrada=Entry(busqueda, bd=5)
@@ -109,7 +101,7 @@ def autor():
     conn.close()
 
 def fecha():
-    conn = sqlite3.connect('noticias.db')
+    conn = sqlite3.connect('derecho.db')
     busqueda = Toplevel()
     label= Label(busqueda, text="Introduzca la fecha(dd/mm/yyyy):")
     entrada=Entry(busqueda, bd=5)
@@ -129,7 +121,7 @@ def fecha():
     conn.close()
 
 def tema():
-    conn = sqlite3.connect('noticias.db')
+    conn = sqlite3.connect('derecho.db')
     busqueda = Toplevel()
     label= Label(busqueda, text="Introduzca palabra clave:")
     entrada=Entry(busqueda, bd=5)
