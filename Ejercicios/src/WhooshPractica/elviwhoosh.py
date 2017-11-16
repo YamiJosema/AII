ad0ba4524422e791a17ff29cbf0186a0d09fdb32
elvgarrui <elviragruiz@gmail.com>
2017-11-16 19:22:04 +0100

    Versión de la practica de Whoosh de elvira terminada.

--- Ejercicios/src/WhooshPractica/elviwhoosh.py
+++ Ejercicios/src/WhooshPractica/elviwhoosh.py
@@ -1,25 +1,35 @@
 #encoding:utf-8
-from Tkinter import Menu
+from Tkconstants import LEFT, END, RIGHT, TOP, Y, BOTH
+from Tkinter import Menu, Entry, Label, Frame, Scrollbar, Listbox, Toplevel
 import Tkinter
 import os
 import tkMessageBox
 
 from bs4 import BeautifulSoup
 import requests
-from whoosh.fields import Schema, TEXT, DATETIME
-from whoosh.index import create_in
+from whoosh.fields import Schema, TEXT
+from whoosh.index import create_in, open_dir
+from whoosh.qparser import QueryParser
 
+
+dirindex1="Index/Temas"
+dirindex2="Index/Respuestas"
 
 def donothing():
-    print "jaja si soi io er chocu"
+    print "boo!"
 
 def index():
-    schema=Schema(titulo=TEXT(stored=True), linktema=TEXT, username=TEXT(stored=True), linkuser=TEXT, texto=TEXT(stored=True), fecha=TEXT)
-
-    if not os.path.exists("index"):
-        os.mkdir("index")
-    ix = create_in("index", schema)
-    writer = ix.writer()
+    schema=Schema(titulo=TEXT(stored=True), linktema=TEXT, username=TEXT(stored=True), linkuser=TEXT, res=TEXT,vis=TEXT, fecha=TEXT(stored=True))
+    if not os.path.exists(dirindex1):
+        os.mkdir(dirindex1)
+    ix1 = create_in(dirindex1, schema)
+    schema=Schema(titulo=TEXT(stored=True), fecha=TEXT(stored=True), username=TEXT(stored=True),linkuser=TEXT,texto=TEXT)
+    if not os.path.exists(dirindex2):
+        os.mkdir(dirindex2)
+    ix2 = create_in(dirindex2, schema)
+    
+    
+    writer = ix1.writer()
     count = 0
     
     for i in range(1,4,1):
@@ -36,26 +46,76 @@ def index():
         
             second = t.find("ul",{"class":"threadstats"})
             
-            res=second.find("a").get_text().strip()
-            vis=second.get_text().split("\n")[2].replace("Visitas: ", "")
+            results=second.find("a").get_text().strip()
+            visits=second.get_text().split("\n")[2].replace("Visitas: ", "")
         
             third=t.find("span",{"class":"time"}).parent
             
             date = third.get_text().strip()
-            autor = t.find("a",{"class":"username"}).get_text()
+            autor = t.find("a",{"class":"username"}).get_text().strip()
             autorlink = "https://foros.derecho.com/"+t.find("a",{"class":"username"})["href"]
             
             r=requests.get(link)
             data = r.text
             soup = BeautifulSoup(data, "lxml")
-            texto = soup.find("blockquote").get_text().strip()
-            print title + "\n" + date + "\n" + autor + "\n" + link + "\n" + autorlink + "\n" + texto + "\n AAAAAAAAAAAA" "\n" 
-            writer.add_document(titulo=unicode(title), linktema=unicode(link), username=unicode(autor), linkuser=unicode(autorlink), texto=unicode(texto), fecha=unicode(date))
+#            print str(count) + ": "+title+"   //  "+autor
+            writer.add_document(titulo=unicode(title).strip(), linktema=unicode(link).strip(), username=unicode(autor).strip(), linkuser=unicode(autorlink).strip(), res=unicode(results).strip(), vis=unicode(visits).strip(), fecha=unicode(date).strip())
             count+=1
-
-    
+            indexres(link,title,ix2)
     
     tkMessageBox.showinfo( "Informacion", "Numero de elementos: "+str(count))
+    writer.commit()
+
+def indexres(url,title,ix):
+    r=requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, "lxml")
+    posts = soup.find_all("li",{"class":"postbitlegacy"})
+    writer2 = ix.writer()
+
+    for p in posts:
+        
+        date = p.find("span",{"class":"date"}).get_text()
+        user = p.find("div",{"class":"username_container"})
+        text = p.find("blockquote").get_text().strip()
+        name = user.strong.get_text().strip()
+        linkuser = "https://foros.derecho.com/"+user.a['href']
+        writer2.add_document(titulo=unicode(title).strip(), fecha=unicode(date), username=unicode(name), linkuser=unicode(linkuser).strip(),texto=unicode(text))
+
+    writer2.commit()
+
+def buscar(pattern,index):
+    def listar(event):
+        lista.delete(0,END)
+        ix=open_dir(index)
+        with ix.searcher() as searcher:
+            query = QueryParser(pattern,ix.schema).parse(unicode(entrada.get()))
+            results= searcher.search(query)
+            for r in results:
+                lista.insert(END,r['titulo'])
+                lista.insert(END,r['fecha'])
+                lista.insert(END,r['username'])
+                lista.insert(END,'')
+                
+    root = Tkinter.Toplevel()
+    frame1=Frame(root)
+    entrada=Entry(frame1,bd=2,width=60)
+    lab = Label(frame1,text="Buscar: ")
+    
+    entrada.bind("<Return>",listar)
+    sc=Scrollbar(root)
+    sc.pack(side=RIGHT,fill=Y)
+    lista=Listbox(root,yscrollcommand=sc.set)
+    frame1.pack(side=TOP)
+
+    lista.pack(side = TOP, fill = BOTH)
+    lab.pack(side = LEFT)
+    entrada.pack(side = LEFT)
+    sc.config(command=lista.yview)
+    root.mainloop()
+    
+
+    
 
 
 def principal():
@@ -70,11 +130,11 @@ def principal():
     bm = Menu(menubar,tearoff=0)
     
     tbm = Menu(bm,tearoff=0)
-    tbm.add_command(label= "Titulo",command = donothing)
-    tbm.add_command(label= "Autor", command = donothing)
+    tbm.add_command(label= "Titulo",command = lambda: buscar("titulo",dirindex1))
+    tbm.add_command(label= "Autor", command = lambda: buscar("username",dirindex1))
     bm.add_cascade(label= "Temas", menu = tbm)
     rbm = Menu(bm,tearoff=0)
-    rbm.add_command(label= "Texto",command = donothing)
+    rbm.add_command(label= "Texto",command = lambda:buscar("texto",dirindex2))
     bm.add_cascade(label= "Respuestas", menu = rbm)
     menubar.add_cascade(label="Buscar", menu = bm)
     top.config(menu= menubar)
